e07fc0c246b301d0d3b772413e10b18e86da95b6
davidstellini <davste93@gmail.com>
2020-04-27 12:58:52 +0300

    Remove redundant model

--- src/app/feed/jackpots/model.ts
+++ src/app/feed/jackpots/model.ts
@@ -1,16 +1,3 @@
-export interface Game {
-  categories: GameCategoryEnum[];
-  name: string;
-  image: string;
-  id: string;
-}
-
-export enum GameCategoryEnum {
-  Top = 'top',
-  Slots = 'slots',
-  New = 'new',
-}
-
 export interface Jackpot {
   game: string;
   amount: number;
