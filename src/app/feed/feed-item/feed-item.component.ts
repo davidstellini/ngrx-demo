44c547ddd364906c48880812786a9efe0eef28b6
davidstellini <davste93@gmail.com>
2020-04-27 12:59:13 +0300

    Update feed styles

--- src/app/feed/feed-item/feed-item.component.ts
+++ src/app/feed/feed-item/feed-item.component.ts
@@ -1,12 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Game } from '../games/model';
+import { DomSanitizer, SafeStyle, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-feed-item',
   templateUrl: './feed-item.component.html',
   styleUrls: ['./feed-item.component.scss'],
 })
-export class FeedItemComponent implements OnInit {
-  constructor() {}
+export class FeedItemComponent implements OnInit, OnChanges {
+  public imgError = false;
+
+  @Input() game?: Game = null;
+  private _backgroundImageUrl: SafeUrl;
+  public get backgroundImageUrl() {
+    return this._backgroundImageUrl;
+  }
+
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.game) {
+      const game = changes.game.currentValue as Game;
+
+      this._backgroundImageUrl = this.sanitizer.bypassSecurityTrustUrl(
+        game.image
+      );
+    }
+  }
 }
--- src/app/feed/feed-list/feed-list.component.ts
+++ src/app/feed/feed-list/feed-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { filter } from 'rxjs/operators';
 import { GamesFacade } from '../games/services/games.facade';
 import { JackpotsFacade } from '../jackpots/services/jackpots.facade';
+import { nativeSmoothScroll } from '@smoovy/scroller';
 
 @Component({
   selector: 'app-feed',
@@ -21,5 +22,9 @@ export class FeedListComponent implements OnInit {
   ngOnInit() {
     this.gamesFacade.fetchGames();
     this.jackpotsFacade.fetchJackpots();
+
+    // nativeSmoothScroll({
+    //   element: document.querySelector('scrollable-content'),
+    // });
   }
 }
